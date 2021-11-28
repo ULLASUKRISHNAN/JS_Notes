824e08759fca54a1b46cc1277bdf76744aca7a08
UllasUKrishnan <krishnanullas3@gmail.com>
2021-11-28 09:00:55 +0530

    Array  Methods added in JS

--- ArrayMethods.js
+++ ArrayMethods.js
@@ -67,3 +67,74 @@ console.log(unFlatArr.flat(2));
 /**======================
  *    flatMap()
  *========================**/
+arr1.map((x) => [x * 2]);
+
+console.log(arr1.flatMap((x) => [x * 2]));
+// First maps and then flattens the returned
+
+/**======================
+ *    forEach()
+ *========================**/
+let ratings = [1, 2, 3];
+let sum = 0;
+
+let sumFunction = function (a, b) {
+  return a + b;
+};
+
+ratings.forEach(function (rating) {
+  sum = sumFunction(sum, rating);
+});
+console.log(`The sum is ${sum}`);
+//forEach expects a synchronous function.
+
+/**======================
+ *    Array.from()
+ *========================**/
+
+console.log(Array.from('abcd'));
+console.log(Array.from([1, 2, 3], (x) => x + x));
+//Array.from(arrayLike, mapFn)
+
+/**======================
+ *    includes()
+ *========================**/
+
+console.log('Does the arr includes 2 ? ' + arr.includes(2));
+
+/**======================
+ *    indexOf()
+ *========================**/
+
+console.log('The index of 2 in arr is ' + arr.indexOf(2));
+
+/**======================
+ *    Array.isArray()
+ *========================**/
+
+console.log('Is the given arr is Array ? ' + Array.isArray(arr));
+
+/**======================
+ *    join()
+ *========================**/
+
+const list = ['Fire', 'Air', 'Water'];
+
+console.log(list.join('-'));
+//join(separator)
+
+/**======================
+ *    keys()
+ *========================**/
+
+const iterator = arr.keys();
+
+for (const key of iterator) {
+  console.log(key);
+}
+
+/**======================
+ *    lastIndexOf()
+ *========================**/
+// lastIndexOf(searchElement, fromIndex)
+console.log('The last index of 2 in arr is ' + arr.lastIndexOf('2'));
